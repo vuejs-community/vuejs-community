0e4e95b7e55039edfe113a70c8f8b987a1f064fe
lonewolfyx <olddrivero.king@qq.com>
2026-09-01 22:48:28 +0800

    feat(icon): add icon module project metadata
    - Define project meta with name @nuxt/icon
    - Set category to nuxt with icon tag
    - Specify types as nuxt-module
    - Add github repository link
    - Include npm package link

---
+++ packages/data-nuxt/src/icon.ts
@@ -0,0 +1,13 @@
+import { defineProjectMeta } from '@vue-community/schema'
+
+export default defineProjectMeta({
+  name: '@nuxt/icon',
+  description: '',
+  category: 'nuxt',
+  tags: ['nuxt', 'icon'],
+  types: ['nuxt-module'],
+  links: {
+    github: 'https://github.com/nuxt/icon',
+    npm: 'https://www.npmjs.com/package/@nuxt/icon',
+  },
+})
