e14368383825a20a386d1170d214dfa6641cae6e
lonewolfyx <olddrivero.king@qq.com>
2026-09-03 00:02:51 +0800

    feat(nuxt): add Ant Design Vue Next Nuxt module project metadata
    - Define project meta with name @antdv-next/nuxt
    - Set description as Antdv Next Nuxt module
    - Add category nuxt with tags including nuxt, ui, component
    - Specify types as nuxt-module
    - Include github and npm links for the package
    - Configure module with proper schema definition

---
+++ packages/data-nuxt/src/antdv-next.ts
@@ -0,0 +1,13 @@
+import { defineProjectMeta } from '@vue-community/schema'
+
+export default defineProjectMeta({
+  name: '@antdv-next/nuxt',
+  description: 'Antdv Next Nuxt module.',
+  category: 'nuxt',
+  tags: ['nuxt', 'ui', 'component'],
+  types: ['nuxt-module'],
+  links: {
+    github: 'https://github.com/antdv-next/nuxt',
+    npm: '@antdv-next/nuxt',
+  },
+})
