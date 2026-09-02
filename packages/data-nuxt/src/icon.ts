59eb95696cbfc5d176d4cdca6c6e1893e4ec46e3
lonewolfyx <olddrivero.king@qq.com>
2026-09-02 10:31:21 +0800

    docs(icon): update package description for icon module
    - Add detailed description explaining Icon component features
    - Document support for Iconify, Emojis and custom components
    - Update metadata to better reflect module capabilities

--- packages/data-nuxt/src/icon.ts
+++ packages/data-nuxt/src/icon.ts
@@ -2,7 +2,7 @@ import { defineProjectMeta } from '@vue-community/schema'
 
 export default defineProjectMeta({
   name: '@nuxt/icon',
-  description: '',
+  description: 'The <Icon> component, supporting Iconify, Emojis and custom components.',
   category: 'nuxt',
   tags: ['nuxt', 'icon'],
   types: ['nuxt-module'],
