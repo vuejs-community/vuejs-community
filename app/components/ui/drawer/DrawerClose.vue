2453b2cf3a8ac575a93902b395b8e6b4008c4ff0
lonewolfyx <olddrivero.king@qq.com>
2026-09-09 14:14:28 +0800

    feat(ui): add accordion, avatar, drawer, form and overlay components

---
+++ app/components/ui/drawer/DrawerClose.vue
@@ -0,0 +1,15 @@
+<template>
+  <DrawerClose
+    data-slot="drawer-close"
+    v-bind="props"
+  >
+    <slot />
+  </DrawerClose>
+</template>
+
+<script lang="ts" setup>
+import type { DrawerCloseProps } from 'reka-ui'
+import { DrawerClose } from 'reka-ui'
+
+const props = defineProps<DrawerCloseProps>()
+</script>
---
+++ app/components/ui/drawer/DrawerTrigger.vue
@@ -0,0 +1,15 @@
+<template>
+  <DrawerTrigger
+    data-slot="drawer-trigger"
+    v-bind="props"
+  >
+    <slot />
+  </DrawerTrigger>
+</template>
+
+<script lang="ts" setup>
+import type { DrawerTriggerProps } from 'reka-ui'
+import { DrawerTrigger } from 'reka-ui'
+
+const props = defineProps<DrawerTriggerProps>()
+</script>
