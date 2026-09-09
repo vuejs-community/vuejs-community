ddd14b6adab080fc33543d81b600baec72d552d8
OldDriver <olddrivero.king@qq.com>
2026-09-09 16:38:02 +0800

    feat(data-ui): add antdv-next (#48)
    * feat(data-ui): add antdv-next
    
    * chore: update index.db

---
+++ packages/data-ui/src/antdv-next.ts
@@ -0,0 +1,28 @@
+import { defineProjectMeta } from '@vuejs-community/schema'
+
+export default defineProjectMeta({
+  name: 'antdv-next',
+  description: '基于 Ant Design v6 设计系统的 Vue 3 企业级组件库',
+  icon: 'icon:antdv-next',
+  category: 'ui',
+  types: ['ui-library'],
+  tags: ['ui', 'ant-design'],
+
+  source: {
+    github: 'antdv-next/antdv-next',
+    npm: 'antdv-next',
+  },
+
+  links: {
+    github: 'https://github.com/antdv-next/antdv-next',
+    npm: 'https://www.npmjs.com/package/antdv-next',
+    website: 'https://www.antdv-next.com',
+  },
+  stats: {
+    stars: 928,
+    downloads: {
+      monthly: 15479,
+      weekly: 5102,
+    },
+  },
+})
